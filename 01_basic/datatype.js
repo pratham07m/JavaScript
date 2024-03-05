0ad76485287cdfd566a5b42a325652b8609e7a06
pratham07m <118175804+pratham07m@users.noreply.github.com>
2024-03-05 17:57:45 +0000

    datatype conversions

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -33,6 +33,9 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+const id = Symbol('123') // this is symbol datatype
+const anotherId = Symbol('123')
+
       // *********************** Operations ***********************
 
 let value = 3
@@ -73,3 +76,30 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+           // datatype more detail
+
+           //  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+
+  // Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  objec */
