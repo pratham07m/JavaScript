e4c2973ae640cdef5426da117752b5a57c513702
Pratham_Mahant <118175804+pratham07m@users.noreply.github.com>
2024-03-13 13:13:03 +0000

    maps in JS

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,12 @@
+ // maps in JS & how to merge multipel map & filter function
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
