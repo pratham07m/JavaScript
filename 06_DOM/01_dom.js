1ba8f325ae1fbb3228438b33f17841a4ade057ec
Pratham_Mahant <118175804+pratham07m@users.noreply.github.com>
2024-03-14 18:15:37 +0000

    DOM in JS

---
+++ 06_DOM/01_dom.js
@@ -0,0 +1,51 @@
+ 
+ /*  Above is the Browser supported features:
+    
+   dom is Dcument object modeling 
+   it use to  Manipulat html using javascript (using dom we can accec all html document in a javascript)
+
+   in above the ways to select html in javascript
+   
+   1>  Selecting with id :   //ID must be uniq in all time in html document
+      document.getElementById(“myId”)  // using by html "ID" attribute 
+    
+   2> Selecting with class :   //class are same in html document
+      document.getElementByClassName("myclass") // using by  html "class" attribute
+
+   3> Selecting with tag : // select using tag name EX - 'p' , 'h1' , .......
+      document.getElementsByTagName(“p”)
+
+    4> Query selector
+       document.querySelector("#id/.class/'tag name'") //thy select all atributes querySelector return first elemrnt of html decument
+
+        document.querySelectorAll("#id/.class/'tag name'")//querySelectorAll returns a all node list of html decument
+        
+
+    
+      
+   
+ 
+ 
+ 
+ */
+
+    /*
+    1 >  differens in innerText , innerHTML , textContent 
+
+         . in innerText we can acces text of element 
+         . in innerHTML we can acces HTML code of the text element
+         . in innerContent we can acces heddin element + text element  "heddin using css heddin property'display:none'"
+
+
+   .style applied
+
+    title.style.backgroundColor = 'red'
+    title.style.borderRadius = "15px"
+
+ 
+     covert into Array
+
+     const myConveredArr = Array.from(class name)
+
+      
+      */
